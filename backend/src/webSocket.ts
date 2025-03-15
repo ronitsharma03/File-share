26c43d32e4975cd7026bf4e953782f47a4d41e2f
Ronit <ronitkhajuria03@gmail.com>
2025-03-15 21:32:30 +0530

    updated webrtc.ts for message handeling

---
+++ backend/src/webSocket.ts
@@ -0,0 +1,59 @@
+import { WebSocketServer, WebSocket } from "ws";
+import { Server as HttpServer } from "http";
+import url from "url";
+import { v4 as uuidv4 } from "uuid";
+import roomManager from "./webSocket/rooms/roomManager";
+import { handleMessages } from "./webSocket/handlers/messageHandler";
+
+const setupWebSocketServer = (server: HttpServer): WebSocketServer => {
+  const wss = new WebSocketServer({ server });
+
+  wss.on("connection", (ws: WebSocket, req) => {
+    const params = url.parse(req.url || "", true).query;
+    const roomId = params.roomId as string;
+    const clientId = (params.clientId as string) || uuidv4();
+
+    const pingInterval = setInterval(() => {
+      if (ws.readyState === WebSocket.OPEN) {
+        ws.ping();
+      }
+    }, 30000);
+
+    ws.on("message", (data: Buffer) => {
+      try {
+        const message = JSON.parse(data.toString());
+        handleMessages(ws, message);
+        console.log(`Message: ${message}`);
+      } catch (error) {
+        console.error(`Error: ${error}`);
+      }
+    });
+
+    ws.on("close", () => {
+      clearInterval(pingInterval);
+
+      roomManager.removeClientFromRoom(ws);
+
+      console.log("Client got disconnected");
+    });
+
+    if (roomId) {
+      roomManager.addClientToRoom(ws, roomId, clientId);
+
+      ws.send(
+        JSON.stringify({
+          type: "room-join",
+          roomId,
+          clients: roomManager.getClientIdsInRoom(roomId),
+        })
+      );
+    }
+    ws.on("error", () => {
+      console.error(`Error setting ws server`);
+    });
+  });
+
+  return wss;
+};
+
+export default setupWebSocketServer;
