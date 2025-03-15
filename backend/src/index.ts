1445da6d2bb411b303cb2052970561d895e04ee4
Ronit <ronitkhajuria03@gmail.com>
2025-03-15 21:31:27 +0530

    update index.ts to handle ws messages

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,8 @@
 import express from "express";
 import http from "http";
-import { WebSocketServer } from "ws";
 import cors from "cors";
 import { config } from "dotenv";
+import setupWebSocketServer from "./webSocket";
 config();
 
 const app = express();
@@ -11,20 +11,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 
-const wss = new WebSocketServer({ server });
-wss.on("connection", (ws) => {
-  ws.on("error", console.error);
 
-  ws.on("message", (data: Buffer) => {
-    const message = data.toString()
-    console.log(`Meessage: ${message}`);
-  });
-
-  ws.on("close", () => {
-    console.log("Client disconnected");
-  });
-
-});
 
 app.get("/api/v1/health", (req, res) => {
   res.status(200).json({
@@ -33,5 +20,10 @@ app.get("/api/v1/health", (req, res) => {
 });
 
 server.listen(PORT, () => {
+  try{
+    setupWebSocketServer(server);
+  }catch(error){
+    console.error(`Error starting WS server: ${error}`);
+  }
   console.log(`Server listening on http://localhost:${PORT}`);
 });
